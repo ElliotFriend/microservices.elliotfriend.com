a1e9c409e0ae1639e091d50dd893822803ff0bf4
Elliot J. Voris <elliot@voris.me>
2021-08-12 11:14:23 -0500

    Removing the old 'users' route from the starter package.
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/timestamp', timestampRouter);
 
 // catch 404 and forward to error handler
